87b6b0cf603aa471ac82ff10f7716d2c23b2fec1
Ritik Kumar <ritikkr274@gmail.com>
2026-07-04 09:04:17 +0530

    bl

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -7,7 +7,9 @@ const isOAuth2Configured = process.env.EMAIL_USER && process.env.CLIENT_ID && pr
 
 if (isOAuth2Configured) {
   transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
       type: 'OAuth2',
       user: process.env.EMAIL_USER,
